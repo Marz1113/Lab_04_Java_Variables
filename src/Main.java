a8b7f3ca891211962bd554eb02a2de1dfd4d0ac8
marzu <marzuqazam06@gmail.com>
2024-09-22 13:49:34 -0400

    final

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,68 @@
+import com.sun.org.apache.xpath.internal.objects.XString;
+
 public class Main
 {
     public static void main(String[] args)
     {
-      
+      int intOperandA = 31;
+      int intOperandB = 13;
+
+      int intSum = 0;
+      int intProduct = 0;
+      int intDifference = 0;
+      int intQuotient = 0;
+      int intModulo = 0;
+
+      double doubleOperandA = 24.24;
+      double doubleOperandB = 3.15;
+
+      double doubleSum = 0;
+      double doubleProduct = 0;
+      double doubleDifference = 0;
+      double doubleQuotient = 0;
+      double doubleModulo = 0;
+
+      intSum = intOperandA + intOperandB;
+        System.out.println("the sum using two ints of " + intOperandA + " and " + intOperandB + " is " + intSum);
+
+      intProduct = intOperandA * intOperandB;
+        System.out.println("the product using two ints of " + intOperandA + " and " + intOperandB + " is " + intProduct);
+
+      intDifference = intOperandA - intOperandB;
+        System.out.println("the Difference using two ints of " + intOperandA + " and " + intOperandB + " is " + intDifference);
+
+      intQuotient = intOperandA/intOperandB;
+      System.out.println("the Quotient using two ints of " + intOperandA + " and " + intOperandB + " is " + intQuotient);
+
+      intModulo = intOperandA % intOperandB;
+      System.out.println("the Modulo using two ints of " + intOperandA + " and " + intOperandB + " is " + intModulo);
+
+
+      doubleSum = doubleOperandA + doubleOperandB;
+      System.out.println("the sum using two ints of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleSum);
+
+      doubleProduct = doubleOperandA * doubleOperandB;
+      System.out.println("the product using two ints of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleProduct);
+
+      doubleDifference = doubleOperandA - doubleOperandB;
+      System.out.println("the difference using two ints of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleDifference);
+
+      doubleQuotient = doubleOperandA / doubleOperandB;
+      System.out.println("the quotient using two ints of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleQuotient);
+
+      doubleModulo = doubleOperandA % doubleOperandB;
+      System.out.println("the Modulo using two ints of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleModulo);
+
+
+      double lunchCost = 15.50;
+      int kidsInFam = 3;
+      boolean isItRaining = false;
+      int favNum = 14;
+      double shoeSize = 9.5;
+      String MarzuqAzam;
+
+
+
+
     }
 }
